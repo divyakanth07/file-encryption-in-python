d0a7742b3509a6f984684f4a9775999b75f5ae81
divykanth07 <divyabonkey@gmail.com>
2025-03-12 10:45:55 +0530

    added better_encryption.py which uses aes to encrypt the date then encrypted thes the aes key with rsa and then we do the reverse for decryption

--- better_encryption.py
+++ better_encryption.py
@@ -2,7 +2,7 @@ import os
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 
 def encrypt_file(rsa, input_path, output_path):
     secret_key = get_random_bytes(32)
@@ -23,4 +23,28 @@ def encrypt_file(rsa, input_path, output_path):
             encrypted_data = aes_engine.encrypt(pad(input_data, AES.block_size))
             output.write(encrypted_data)
 
-encrypt_file(RSA.generate(2048), 'test.txt', 'encryptedfile')
+def decrypt_file(rsa, input_path, output_path):
+    with open(input_path, 'rb') as input_file:
+        encrypted_key_len = int.from_bytes(input_file.read(4), byteorder='big')
+        encrypted_secret_key = input_file.read(encrypted_key_len)
+
+        cipher_rsa = PKCS1_OAEP.new(rsa)
+        secret_key = cipher_rsa.decrypt(encrypted_secret_key)
+
+        iv = input_file.read(16)
+        aes_engine = AES.new(secret_key, AES.MODE_CBC, iv)
+
+        encrypted_data = input_file.read()
+        decrypted_data = unpad(aes_engine.decrypt(encrypted_data), AES.block_size)
+
+        with open(output_path, 'wb') as output_file:
+            output_file.write(decrypted_data)
+
+def save_rsa_key(rsa, key_path):
+	with open(key_path, 'wb') as key_file:
+		key_file.write(rsa.export_key())
+
+rsa_key = RSA.generate(2048)
+encrypt_file(rsa_key, 'test.txt', 'encryptedfile')
+decrypt_file(rsa_key, 'encryptedfile', 'decrypted_test.txt')
+save_rsa_key(rsa_key, 'rsakey.txt')
